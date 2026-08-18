34b66d5d8eb0b2f76b8ec8d7cfb8143548063fd8
kazakanan <ncw@uw.edu>
2026-08-18 15:14:59 -0700

    More updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,5 @@
 import { setupToggle } from "./toggle.js";
 import { setupModal } from "./modal.js";
-import { radioGroup } from "./radio-grp.js";
 
 setupToggle("toggle-button", "content-to-toggle");
 setupModal("modal", "open-modal-button", "close-modal-button");
-radioGroup();
